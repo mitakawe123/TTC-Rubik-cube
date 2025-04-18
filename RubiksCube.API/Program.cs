274f5dc7ec9598c29d199a9848421f4ae0bbbf08
mitakawe123 <dkyuchukov2003@gmail.com>
2025-04-18 16:28:28 +0300

    add swagger support

--- RubiksCube.API/Program.cs
+++ RubiksCube.API/Program.cs
@@ -3,16 +3,17 @@ using RubiksCube.Application.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCoreServices(); 
-
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.UseHttpsRedirection();
-
 app.Run();
